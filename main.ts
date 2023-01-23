2b61494f1c8842eca1e6ea2ea70ad5c2d87db9ec
rickyenhannah <123379577+rickyenhannah@users.noreply.github.com>
2023-01-23 15:58:44 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,9 @@
+let stappen = 0
+input.onButtonPressed(Button.A, function () {
+    stappen = 0
+    basic.showNumber(stappen)
+})
+input.onGesture(Gesture.ThreeG, function () {
+    stappen += 1
+    basic.showNumber(stappen)
+})
